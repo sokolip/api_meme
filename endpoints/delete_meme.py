01ec4fe2f9e28863ece6717d24833f1a83984bb1
sokolip <sokolip8@gmail.com>
2024-09-03 10:37:25 -0500

    fixed issues with fixture

--- endpoints/delete_meme.py
+++ endpoints/delete_meme.py
@@ -4,18 +4,6 @@ from endpoints.endpoint import Endpoint
 
 
 class DeleteMeme(Endpoint):
-    @allure.step('Create new meme')
-    def new_meme(self, body, headers):
-        self.response = requests.post(
-            url=f'{self.url}/meme',
-            json=body,
-            headers=headers
-        )
-        assert self.response.status_code == 200, 'Meme does not created'
-        response = self.response.json()
-        id_meme = response['id']
-        print(f'Created new meme with id = {id_meme}')
-        return id_meme
 
     @allure.step('Delete meme')
     def delete_meme(self, id_meme, headers):
@@ -25,3 +13,12 @@ class DeleteMeme(Endpoint):
         )
         assert self.response.status_code == 200, 'Meme do not deleted'
         print(f'Meme with id = {id_meme} deleted')
+
+    @allure.step('Delete meme with alien token')
+    def delete_meme_with_alien_token(self, id_meme, headers):
+        self.response = requests.delete(
+            url=f'{self.url}/meme/{id_meme}',
+            headers=headers
+        )
+        print(self.response.status_code)
+        assert self.response.status_code == 401, 'Check token'
